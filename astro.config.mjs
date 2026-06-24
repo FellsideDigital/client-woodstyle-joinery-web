0ad19ada680320820504e36b68427ea2a78ecfd9
oliver-lewington <oliver.a.lewington@gmail.com>
2026-06-24 14:13:49 +0100

    Moving to tailwind css, and updating certain marketing areas and design

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   site: 'https://www.woodstylejoinery.co.uk',
   integrations: [sitemap()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
